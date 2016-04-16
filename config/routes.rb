022629e77597c7b5b75461f13452e960cf6f1efc
tanky <vagrant@rails-carrier>
2016-04-16 08:36:55 +0000

    end of ROR class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :gram
+  resources :grams
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
